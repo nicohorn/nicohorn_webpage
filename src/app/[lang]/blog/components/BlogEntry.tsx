c9c72e68a88536ee98535180d140f9aee639ee71
nicohorn <nicoskate000@gmail.com>
2024-03-18 18:28:52 -0300

    Breakpoint for text size in Blog Entry title

--- src/app/[lang]/blog/components/BlogEntry.tsx
+++ src/app/[lang]/blog/components/BlogEntry.tsx
@@ -116,7 +116,7 @@ export default function BlogEntry({
         <div id="blog_hero" className="mx-auto flex flex-col gap-2 p-5">
           <h1
             id="blog_title"
-            className="text-left text-7xl font-extrabold transition text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-yellow-600"
+            className="text-left md:text-7xl text-5xl font-extrabold transition text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-yellow-600"
           >
             {blog_entry?.title}
           </h1>
