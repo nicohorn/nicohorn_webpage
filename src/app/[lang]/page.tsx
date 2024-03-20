1151133ea59b7f76063e6da967633a3b48fad467
nicohorn <nicoskate000@gmail.com>
2024-03-20 17:12:12 -0300

    Landing page 2.0

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -3,8 +3,7 @@
 import Title from "@/components/Title";
 import Image from "next/image";
 import Editor from "@monaco-editor/react";
-import { vscodeDark } from "@uiw/codemirror-theme-vscode";
-
+import { motion } from "framer-motion";
 export default function Page({}) {
   const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -32,8 +31,24 @@ export default function Page({}) {
 
   return (
     <main className=" w-full flex flex-col gap-3">
-      <Title title="Nico Horn" />
-      <div className="flex gap-2">
+      <div className="flex gap-2 items-end">
+        <Title title="Nico Horn" />
+        <motion.a
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.5 }}
+          className="-translate-y-5"
+          href="mailto:contact@nicohorn.com"
+        >
+          contact@nicohorn.com
+        </motion.a>
+      </div>
+
+      <motion.div
+        initial={{ x: -10, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ delay: 0.5 }}
+      >
         <Editor
           width="500px"
           height="100px"
@@ -41,24 +56,26 @@ export default function Page({}) {
           theme="vs-dark"
           value={description}
         />
-        <div>
-          {" "}
-          <div className=" relative w-[20rem] min-h-[40vh]">
-            <Image
-              placeholder={`data:image/svg+xml;base64,${toBase64(
-                shimmer(800, 800)
-              )}`}
-              layout="fill"
-              className="rounded-2xl border object-cover"
-              alt="Profile picture of Nico Horn"
-              src="https://images.unsplash.com/photo-1708461901625-4fb5aa1e9265?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            ></Image>
-          </div>{" "}
-          <a href="mailto:contact@nicohorn.com">contact@nicohorn.com</a>
-        </div>
+      </motion.div>
+      <div>
+        {" "}
+        <div className=" relative w-[20rem] min-h-[40vh]">
+          <Image
+            placeholder={`data:image/svg+xml;base64,${toBase64(
+              shimmer(800, 800)
+            )}`}
+            layout="fill"
+            className="object-cover"
+            alt="Profile picture of Nico Horn"
+            src="https://images.unsplash.com/photo-1708461901625-4fb5aa1e9265?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          ></Image>
+        </div>{" "}
       </div>
+    </main>
+  );
+}
 
-      {/* <div className="place-self-end shadow-lg">
+/* <div className="place-self-end shadow-lg">
         <a
           className="twitter-timeline"
           data-width="350"
@@ -74,7 +91,4 @@ export default function Page({}) {
       <iframe
         className="w-full aspect-video bg-white"
         src="https://fastidious-lamington-ed3fd8.netlify.app/"
-      ></iframe> */}
-    </main>
-  );
-}
+      ></iframe> */
