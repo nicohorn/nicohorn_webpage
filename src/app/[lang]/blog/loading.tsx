927b4b9afd3bc89620eae433f6d942a9efd7a2ee
nicohorn <nicoskate000@gmail.com>
2024-03-21 20:30:00 -0300

    Fixed build error

--- src/app/[lang]/blog/loading.tsx
+++ src/app/[lang]/blog/loading.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import SquareLoader from "@/components/SquareLoader";
-import { BarLoader } from "react-spinners";
 
 export default function Loading() {
   return <SquareLoader />;
