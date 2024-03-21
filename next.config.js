efee71ab81d8928b3344786ca7f43214aaa7feed
nicohorn <nicoskate000@gmail.com>
2024-03-21 19:17:24 -0300

    Finishing up landing page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["images.unsplash.com"]
+        domains: ["images.unsplash.com", "scontent-eze1-1.xx.fbcdn.net"]
     }
 
 }
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -6,9 +6,19 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
-import { dictLinksToEnglish } from "../Links";
 import Link from "next/link";
+import Script from "next/script";
 export default function Page({ params }: { params: { lang: string } }) {
+  const images = [
+    "https://scontent-eze1-1.xx.fbcdn.net/v/t39.30808-6/292958230_10209333234045073_9217687885836746361_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGUY8VnGOB9GTCuu6KaJYkNupO4-4YEv2m6k7j7hgS_aWwBjaOSWoOeKvRmoenj7NY&_nc_ohc=snRsaM4BtSMAX-HzXaT&_nc_ht=scontent-eze1-1.xx&oh=00_AfC5a768EFzPH0Tz_jgeIe2uAuvC176Ik8SfdVtdj6gipQ&oe=66024FC6",
+    "https://images.unsplash.com/photo-1708461901625-4fb5aa1e9265?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1625241189662-2980453ebffc?q=80&w=2081&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1527980965255-d3b416303d12?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://scontent-eze1-1.xx.fbcdn.net/v/t39.30808-6/322938186_516788973777185_6854656873251529764_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHdjQJOSP6RW9k13PJ-p_cU5cHe0mXLuaLlwd7SZcu5om5yiXOqf6gfJVOI9YkHfQU&_nc_ohc=0ZLrCzRMTpIAX-vwv2z&_nc_ht=scontent-eze1-1.xx&oh=00_AfC_9giEBUFbffOGSu-hb2xLcCjXm8iB0uWDlGRfz_XSkw&oe=6601988B",
+  ];
+
+  const [selectedImage, setSelectedImage] = useState(0);
+
   const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
@@ -74,7 +84,7 @@ export default function Page({ params }: { params: { lang: string } }) {
                 setLinkBgOpacity(0);
               }, 75);
             }}
-            className="relative flex gap-3 "
+            className="relative flex gap-6 "
           >
             <div
               className="pointer-events-none absolute -z-10 hidden border-yellow-400 bg-yellow-600 duration-150 lg:block"
@@ -122,7 +132,22 @@ export default function Page({ params }: { params: { lang: string } }) {
                   : " font-semibold"
               }`}
             >
-              {params.lang == "en-US" ? dictLinksToEnglish["Blog"] : "Blog"}
+              Blog
+            </Link>
+            <Link
+              onMouseOver={() => {
+                setLinkBgOpacity(1);
+                setHoverLink("cool_stuff");
+              }}
+              id={`link_cool_stuff`}
+              href={`/${params.lang}/cool_stuff`}
+              className={`z-10 w-fit py-1 text-xl uppercase transition delay-75 duration-150 lg:py-0 ${
+                path === `/${params.lang}/blog`
+                  ? "border-b border-b-yellow-400 font-bold"
+                  : " font-semibold"
+              }`}
+            >
+              {params.lang == "en-US" ? "Cool stuff" : "Cosas copadas"}
             </Link>
           </div>
         </motion.p>
@@ -134,34 +159,40 @@ export default function Page({ params }: { params: { lang: string } }) {
             className="relative min-h-[40vh] w-[20rem]"
           >
             <Image
+              onClick={() => {
+                selectedImage == images.length - 1
+                  ? setSelectedImage(0)
+                  : setSelectedImage(selectedImage + 1);
+              }}
               placeholder={`data:image/svg+xml;base64,${toBase64(
                 shimmer(800, 800),
               )}`}
               layout="fill"
-              className="object-cover"
+              className="cursor-pointer object-cover"
               alt="Profile picture of Nico Horn"
-              src="https://images.unsplash.com/photo-1708461901625-4fb5aa1e9265?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              src={images[selectedImage]}
             ></Image>
           </motion.div>
+          <div className="place-self-end shadow-lg">
+            <a
+              className="twitter-timeline"
+              data-width="350"
+              data-height="500"
+              data-theme="dark"
+              href="https://twitter.com/NicoTheEngineer?ref_src=twsrc%5Etfw"
+            >
+              Tweets by NicoTheEngineer
+            </a>
+          </div>
+
+          <Script async src="https://platform.twitter.com/widgets.js" />
         </div>
       </div>
     </main>
   );
 }
 
-/* <div className="place-self-end shadow-lg">
-        <a
-          className="twitter-timeline"
-          data-width="350"
-          data-height="500"
-          data-theme="dark"
-          href="https://twitter.com/NicoTheEngineer?ref_src=twsrc%5Etfw"
-        >
-          Tweets by NicoTheEngineer
-        </a>
-      </div>
-
-      <Script async src="https://platform.twitter.com/widgets.js" />
+/* 
       <iframe
         className="w-full aspect-video bg-white"
         src="https://fastidious-lamington-ed3fd8.netlify.app/"
